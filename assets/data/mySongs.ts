429d41456e675b3e63f47c2ae1f6004afd93c34f
joseblotta <52714385+joseblotta@users.noreply.github.com>
2019-07-23 15:19:17 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,34 @@
+import { Songs } from '../../app/songInfo';
+
+export const MYSONGS: Songs[] = [
+    {
+        songName: "Two Hearts",
+        artist: "Phil Collins",
+        genre: "Pop",
+        yearRelease: 1988,
+        picture: 'TwoHeartsPhilCollins.jpg'
+    },
+    {
+        songName: "Everybody Wants to Rule the World",
+        artist: "Tears for Fears",
+        genre: "Pop",
+        yearRelease: 1985,
+        picture: 'EverybodyWantstoRuletheWorld.jpg' 
+    },
+    {
+        songName: "We Are the Champions",
+        artist: "Queen",
+        genre: "Classic Rock, Rock",
+        yearRelease: 1977,
+        picture: 'WeAretheChampions.jpg' 
+    },
+    {
+        songName: "Symphony No. 9",
+        artist: "Ludwig Van Beethoven",
+        genre: "Classical",
+        yearRelease: 1824,
+        picture: 'SymphonyNo.9.jpg' 
+    }
+]
+
+
